85e8d74d7416c8b13ce6f3275f041e7c3576049d
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-11-27 10:21:26 -0500

    feat: update sendQueryArgs cli

--- js/src/cli/index.ts
+++ js/src/cli/index.ts
@@ -1,50 +1,52 @@
-import { Command } from 'commander';
-import { compile } from './compile';
-import { run } from './run';
-import { sendQuery } from './sendQuery';
+import { Command } from "commander";
+import { compile } from "./compile";
+import { run } from "./run";
+import { sendQuery } from "./sendQuery";
 
 const program = new Command();
 
-program
-    .name("axiom")
-    .version("0.1.0")
-    .description("axiom CLI")
+program.name("axiom").version("0.1.0").description("axiom CLI");
 
 program
-    .command("compile")
-    .description("compile an Axiom circuit")
-    .argument("<circuit path>", "circuit path")
-    .option("-s, --stats", "print stats")
-    .option("-p, --provider [provider]", "provider")
-    .option("-i, --inputs [inputs]", "inputs")
-    .option("-o, --output [output]", "output", "data/build.json")
-    .option("-f, --function [function]", "function name", "circuit")
-    .action(compile)
+  .command("compile")
+  .description("compile an Axiom circuit")
+  .argument("<circuit path>", "circuit path")
+  .option("-s, --stats", "print stats")
+  .option("-p, --provider [provider]", "provider")
+  .option("-i, --inputs [inputs]", "inputs")
+  .option("-o, --output [output]", "output", "data/build.json")
+  .option("-f, --function [function]", "function name", "circuit")
+  .action(compile);
 
 program
-    .command("run")
-    .description("run an Axiom circuit")
-    .argument("<circuit path>", "circuit path")
-    .option("-b, --build [build]", "build path", "data/build.json")
-    .option("-s, --stats", "print stats")
-    .option("-p, --provider [provider]", "provider")
-    .option("-i, --inputs [inputs]", "inputs")
-    .option("-o, --output [output]", "output", "data/output.json")
-    .option("-f, --function [function]", "function name", "circuit")
-    .action(run)
+  .command("run")
+  .description("run an Axiom circuit")
+  .argument("<circuit path>", "circuit path")
+  .option("-b, --build [build]", "build path", "data/build.json")
+  .option("-s, --stats", "print stats")
+  .option("-p, --provider [provider]", "provider")
+  .option("-i, --inputs [inputs]", "inputs")
+  .option("-o, --output [output]", "output", "data/output.json")
+  .option("-f, --function [function]", "function name", "circuit")
+  .action(run);
 
 program
-    .command("sendQuery")
-    .description("get args / calldata necessary to send an Axiom query")
-    .option("-s, --stats", "print stats")
-    .option("-p, --provider [provider]", "provider")
-    .option("-m, --mock", "mock")
-    .option("-c, --calldata", "calldata")
-    .option("-a, --args", "args")
-    .option("-i, --input [input]", "input", "data/output.json")
-    .option("-o, --output [output]", "output", "data/sendQuery.json")
-    .option("-r, --refundAddress [refundAddress]", "refundAddress")
-    .option("--caller [caller]", "caller (defaults to refundAddress)")
-    .action(sendQuery)
+  .command("sendQueryArgs")
+  .description("get args / calldata necessary to send an Axiom query")
+  .argument("<callback address>", "callback address")
+  .option("-c, --calldata", "output encoded calldata")
+  .option("-s, --sourceChainId [sourceChainId]", "source chain id")
+  .option("-r, --refundAddress [refundAddress]", "refundAddress")
+  .option("-p, --provider [provider]", "provider")
+  .option("-e, --callbackExtraData [callbackExtraData]", "callback extra data")
+  .option("--caller [caller]", "caller (defaults to refundAddress)")
+  .option("-i, --input [input]", "circuit run output path", "data/output.json")
+  .option("-m, --mock", "return AxiomV2QueryMock address")
+  .option(
+    "-o, --output [output]",
+    "sendQueryArgs output path",
+    "data/sendQuery.json",
+  )
+  .action(sendQuery);
 
-program.parseAsync(process.argv)
+program.parseAsync(process.argv);
--- js/src/cli/sendQuery.ts
+++ js/src/cli/sendQuery.ts
@@ -2,63 +2,63 @@ import { Axiom } from "@axiom-crypto/core";
 import { getProvider, readJsonFromFile, saveJsonToFile } from "./utils";
 import { buildSendQuery } from "../sendQuery";
 
-export const sendQuery = async (options: {
-    stats: boolean,
-    mock: boolean,
-    calldata: boolean,
-    args: boolean,
-    output: string,
-    input: string,
-    refundAddress: string,
-    caller: string,
-    provider?: string,
-}) => {
-    if (options.args && options.calldata) {
-        throw new Error("Please choose either args or calldata.");
+export const sendQuery = async (
+  callbackAddress: string,
+  options: {
+    refundAddress: string;
+    sourceChainId: string;
+    callbackExtraData: string;
+    calldata: boolean;
+    output: string;
+    input: string;
+    caller: string;
+    provider?: string;
+    mock: boolean;
+  },
+) => {
+  if (!options.refundAddress) {
+    throw new Error("Please provide a refund address.");
+  }
+  if (!options.sourceChainId) {
+    throw new Error("Please provide a source chain ID.");
+  }
+  const provider = getProvider(options.provider);
+  const outputJson = readJsonFromFile(options.input);
+  const axiom = new Axiom({
+    providerUri: provider,
+    chainId: options.sourceChainId,
+    mock: options.mock,
+    version: "v2",
+  });
+  try {
+    let build = await buildSendQuery({
+      axiom,
+      dataQuery: outputJson.dataQuery,
+      computeQuery: outputJson.computeQuery,
+      callback: {
+        target: callbackAddress,
+        extraData: options.callbackExtraData,
+      },
+      refundAddress: options.refundAddress,
+      caller: options.caller ?? options.refundAddress,
+    });
+    build.value = build.value.toString() as any;
+    let res: any;
+    if (!options.calldata) {
+      res = {
+        value: build.value,
+        args: build.args,
+        queryId: build.queryId,
+      };
+    } else {
+      res = {
+        value: build.value,
+        calldata: build.calldata,
+        queryId: build.queryId,
+      };
     }
-    if(!options.refundAddress){
-        throw new Error("Please provide a refund address.");
-    }
-    const provider = getProvider(options.provider);
-    const outputJson = readJsonFromFile(options.input);
-    const axiom = new Axiom({
-        providerUri: provider,
-        chainId: 5,
-        mock: options.mock,
-        version: "v2"
-    })
-    try {
-        let build = await buildSendQuery({
-            axiom,
-            dataQuery: outputJson.dataQuery,
-            computeQuery: outputJson.computeQuery,
-            callback: outputJson.callback,
-            refundAddress: options.refundAddress,
-            caller: options.caller ?? options.refundAddress,
-        })
-        build.value = build.value.toString() as any;
-        let res: any;
-        if (options.args) {
-            res = {
-                value: build.value,
-                args: build.args,
-                queryId: build.queryId,
-            }
-        }
-        else if (options.calldata) {
-            res = {
-                value: build.value,
-                calldata: build.calldata,
-                queryId: build.queryId,
-            }
-        }
-        else {
-            res = build;
-            delete res.abi;
-        }
-        saveJsonToFile(res, options.output, "sendQuery.json");
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
+    saveJsonToFile(res, options.output, "sendQuery.json");
+  } catch (e) {
+    console.error(e);
+  }
+};

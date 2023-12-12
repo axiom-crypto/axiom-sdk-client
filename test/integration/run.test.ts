9c843b9b03a968007b53fd5cbf534a5b355406fd
Yu Jiang Tham <yoojbruin@gmail.com>
2023-12-11 16:55:57 -0800

    Update test timeout, add file checks

--- test/integration/run.test.ts
+++ test/integration/run.test.ts
@@ -1,5 +1,6 @@
 import { buildCircuit } from "./template/buildCircuit";
 import util from 'node:util';
+import fs from 'fs';
 import { listDir, makeFileMap } from "./template/utils";
 
 const exec = util.promisify(require('node:child_process').exec);
@@ -24,22 +25,29 @@ describe("Run", () => {
         const circuitPath = buildCircuit(inputFile);
 
         // Compile the circuit
-        const compileCmd = `npx axiom compile "${circuitPath}" --function circuit --output "${outputFileBase}.build.json" --provider $PROVIDER_URI_GOERLI`;
+        const buildFile = `${outputFileBase}.build.json`;
+        const compileCmd = `npx axiom compile "${circuitPath}" --function circuit --output "${buildFile}" --provider $PROVIDER_URI_GOERLI`;
         const { stdout, stderr: compileStdErr } = await exec(compileCmd);
-        console.log(stdout);
         if (compileStdErr) {
           console.log("Failed on compile command:", compileCmd);
           throw new Error(`Compile error: ${compileStdErr}`);
         }
 
+        // Check build file exists
+        expect(fs.existsSync(buildFile)).toBe(true);
+
         // Run the circuit
-        const runCmd = `npx axiom run "${circuitPath}" --function circuit --build "${outputFileBase}.build.json" --output "${outputFileBase}.output.json" --provider $PROVIDER_URI_GOERLI`;
+        const outputFile = `${outputFileBase}.output.json`;
+        const runCmd = `npx axiom run "${circuitPath}" --function circuit --build "${buildFile}" --output "${outputFile}" --provider $PROVIDER_URI_GOERLI`;
         const { stderr: runStdErr } = await exec(runCmd);
         if (runStdErr) {
           console.log("Failed on run command:", runCmd);
           throw new Error(`Run error: ${runStdErr}`);
         }
-      }, 100000);
+
+        // Check output file exists
+        expect(fs.existsSync(outputFile)).toBe(true);
+      }, 180000);
     }
   }
 });

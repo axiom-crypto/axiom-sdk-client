e93c4400054f4ec03d1072236f95a7f75d8ffb9c
Yu Jiang Tham <yoojbruin@gmail.com>
2023-12-11 16:08:07 -0800

    pnpm -> npx

--- test/integration/run.test.ts
+++ test/integration/run.test.ts
@@ -24,7 +24,7 @@ describe("Run", () => {
         const circuitPath = buildCircuit(inputFile);
 
         // Compile the circuit
-        const compileCmd = `pnpm axiom compile "${circuitPath}" --function circuit --output "${outputFileBase}.build.json" --provider $PROVIDER_URI_GOERLI`;
+        const compileCmd = `npx axiom compile "${circuitPath}" --function circuit --output "${outputFileBase}.build.json" --provider $PROVIDER_URI_GOERLI`;
         const { stderr: compileStdErr } = await exec(compileCmd);
         if (compileStdErr) {
           console.log("Failed on compile command:", compileCmd);
@@ -32,7 +32,7 @@ describe("Run", () => {
         }
 
         // Run the circuit
-        const runCmd = `pnpm axiom run "${circuitPath}" --function circuit --build "${outputFileBase}.build.json" --output "${outputFileBase}.output.json" --provider $PROVIDER_URI_GOERLI`;
+        const runCmd = `npx axiom run "${circuitPath}" --function circuit --build "${outputFileBase}.build.json" --output "${outputFileBase}.output.json" --provider $PROVIDER_URI_GOERLI`;
         const { stderr: runStdErr } = await exec(runCmd);
         if (runStdErr) {
           console.log("Failed on run command:", runCmd);
@@ -41,16 +41,4 @@ describe("Run", () => {
       }, 100000);
     }
   }
-
-  // test("Test account", async () => {
-  //   const circuitPath = buildCircuit("./integration/input/account/7_balance.js");
-
-  //   try {
-  //     const { stdout, stderr } = await exec("pnpm axiom compile ./integration/output/account/7_balance.circuit.ts --function circuit --output ./integration/output/account/7_balance.build.json --provider $PROVIDER_URI_GOERLI");
-  //     // console.log("stdout", stdout);
-  //     // console.log("stderr", stderr);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // });
 });

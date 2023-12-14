eb462c8169c123810081eac35cd8fcb01269e2e1
Yu Jiang Tham <yoojbruin@gmail.com>
2023-12-14 15:02:04 -0800

    Update package structure

--- harness/test/integration/goerli.test.ts
+++ harness/test/integration/goerli.test.ts
@@ -7,15 +7,17 @@ describe("Run", () => {
     throw new Error("`PROVIDER_URI_GOERLI` environment variable must be defined");
   }
 
-  const files = listDir("./integration/input/");
+  const inputBasePath = "./test/integration/input";
+  const outputBasePath = "./test/integration/output";
+  const files = listDir(inputBasePath);
   const fileMap = makeFileMap(files);
 
   for (let [folder, files] of Object.entries(fileMap)) {
     for (let file of files) {
-      const inputFile = `./integration/input/${folder}/${file}`;
+      const inputFile = `${inputBasePath}/${folder}/${file}`;
       const fileName = file.split(".js")[0];
-      const outputFolder = `./integration/output/${folder}`;
-      const outputFileBase = `${outputFolder}/${fileName}`;
+      const outputBasePathType = `${outputBasePath}/${folder}`;
+      const outputFileBase = `${outputBasePathType}/${fileName}`;
 
       test(`Test ${folder}: ${inputFile}`, async () => {
         console.log(`Running test: ${inputFile}`)
@@ -24,7 +26,7 @@ describe("Run", () => {
         await run(
           inputFile,
           {
-            output: outputFolder,
+            output: outputBasePathType,
             function: "circuit",
             provider: process.env.PROVIDER_URI_GOERLI,
           }

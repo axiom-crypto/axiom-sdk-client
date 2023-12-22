1aa82158fe814b6912bf6863195238ee96882922
roshan <19766713+rpalakkal@users.noreply.github.com>
2023-12-23 01:20:58 +0530

    keccak test wip

--- rust/src/tests/keccak.rs
+++ rust/src/tests/keccak.rs
@@ -1,5 +1,5 @@
-use crate::run::aggregation::{agg_circuit_keygen, agg_circuit_run};
-use crate::run::inner::{keygen, run};
+use crate::run::aggregation::{agg_circuit_keygen, agg_circuit_mock, agg_circuit_run};
+use crate::run::inner::{keygen, prove, run};
 use crate::scaffold::AxiomCircuitScaffold;
 use crate::subquery::caller::SubqueryCaller;
 use crate::tests::utils::MyCircuitInput;
@@ -22,83 +22,75 @@ use axiom_eth::{
     keccak::promise::KeccakFixLenCall,
     rlc::circuit::builder::RlcCircuitBuilder,
 };
-use ethers::providers::JsonRpcClient;
+use ethers::providers::{Http, JsonRpcClient};
 use std::sync::{Arc, Mutex};
+use test_case::test_case;
 
-macro_rules! keccak_test {
-    ($subquery_call:ident) => {
-        mod $subquery_call {
-            use super::*;
-            #[derive(Debug, Clone, Default)]
-            struct TestStruct;
-            impl<P: JsonRpcClient> AxiomCircuitScaffold<P, Fr> for TestStruct {
-                type CircuitInput = MyCircuitInput;
-                type VirtualCircuitInput = MyCircuitVirtualInput<Fr>;
+macro_rules! keccak_test_struct {
+    ($struct_name:ident, $subquery_call:ident) => {
+        #[derive(Debug, Clone, Default)]
+        struct $struct_name;
+        impl<P: JsonRpcClient> AxiomCircuitScaffold<P, Fr> for $struct_name {
+            type CircuitInput = MyCircuitInput;
+            type VirtualCircuitInput = MyCircuitVirtualInput<Fr>;
 
-                fn virtual_assign_phase0(
-                    &self,
-                    builder: &mut RlcCircuitBuilder<Fr>,
-                    _range: &RangeChip<Fr>,
-                    subquery_caller: Arc<Mutex<SubqueryCaller<P, Fr>>>,
-                    callback: &mut Vec<HiLo<AssignedValue<Fr>>>,
-                    _inputs: Self::VirtualCircuitInput,
-                ) {
-                    let val = $subquery_call(builder, subquery_caller.clone());
-                    dbg!(val);
-                    callback.push(val);
-                    let a = witness!(builder, Fr::from(1));
-                    let b = witness!(builder, Fr::from(2));
-                    let bytes = SafeTypeChip::unsafe_to_fix_len_bytes_vec(vec![a, b], 2);
-                    let keccak_call = KeccakFixLenCall::new(bytes);
-                    let hilo = subquery_caller
-                        .lock()
-                        .unwrap()
-                        .keccak(ctx!(builder, 0), keccak_call);
-                    callback.push(hilo);
-                }
-            }
-
-            #[test]
-            fn test_circuit() {
-                let base_params = BaseCircuitParams {
-                    k: 12,
-                    num_advice_per_phase: vec![4, 1],
-                    num_lookup_advice_per_phase: vec![1, 1],
-                    num_fixed: 1,
-                    num_instance_columns: 1,
-                    lookup_bits: Some(11),
-                };
-                let params = AxiomCircuitParams::Keccak(RlcKeccakCircuitParams {
-                    keccak_rows_per_round: 20,
-                    rlc: RlcCircuitParams {
-                        base: base_params,
-                        num_rlc_columns: 1,
-                    },
-                });
-                let client = get_provider();
-                let (_vk, pk) = keygen::<_, TestStruct>(client.clone(), params.clone(), None);
-                let output = run::<_, TestStruct>(client, params, None, pk);
-                let agg_circuit_params = AggregationConfigParams {
-                    degree: 20,
-                    num_advice: 23,
-                    num_lookup_advice: 2,
-                    num_fixed: 1,
-                    lookup_bits: 19,
-                };
-                let (_, agg_pk, break_points) =
-                    agg_circuit_keygen(agg_circuit_params, output.snark.clone());
-                agg_circuit_run(
-                    agg_circuit_params,
-                    output.snark,
-                    agg_pk,
-                    break_points,
-                    output.data,
-                );
+            fn virtual_assign_phase0(
+                &self,
+                builder: &mut RlcCircuitBuilder<Fr>,
+                _range: &RangeChip<Fr>,
+                subquery_caller: Arc<Mutex<SubqueryCaller<P, Fr>>>,
+                _callback: &mut Vec<HiLo<AssignedValue<Fr>>>,
+                _inputs: Self::VirtualCircuitInput,
+            ) {
+                $subquery_call(builder, subquery_caller.clone());
+                let a = witness!(builder, Fr::from(1));
+                let b = witness!(builder, Fr::from(2));
+                let bytes = SafeTypeChip::unsafe_to_fix_len_bytes_vec(vec![a, b], 2);
+                let keccak_call = KeccakFixLenCall::new(bytes);
+                subquery_caller
+                    .lock()
+                    .unwrap()
+                    .keccak(ctx!(builder, 0), keccak_call);
             }
         }
     };
 }
 
-keccak_test!(account_call);
+fn get_keccak_test_params() -> AxiomCircuitParams {
+    let params = BaseCircuitParams {
+        k: 12,
+        num_advice_per_phase: vec![4],
+        num_lookup_advice_per_phase: vec![1],
+        num_fixed: 1,
+        num_instance_columns: 1,
+        lookup_bits: Some(11),
+    };
+    let rlc_params = RlcCircuitParams {
+        base: params,
+        num_rlc_columns: 1,
+    };
+    AxiomCircuitParams::Keccak(RlcKeccakCircuitParams {
+        keccak_rows_per_round: 20,
+        rlc: rlc_params,
+    })
+}
 
-keccak_test!(header_call);
+keccak_test_struct!(AccountTest, account_call);
+keccak_test_struct!(HeaderTest, header_call);
+
+#[test_case(AccountTest)]
+#[test_case(HeaderTest)]
+pub fn mock<S: AxiomCircuitScaffold<Http, Fr>>(_circuit: S) {
+    let params = get_keccak_test_params();
+    let agg_circuit_params = AggregationConfigParams {
+        degree: 20,
+        num_advice: 23,
+        num_lookup_advice: 2,
+        num_fixed: 1,
+        lookup_bits: 19,
+    };
+    let client = get_provider();
+    let (_, pk) = keygen::<_, S>(client.clone(), params.clone(), None);
+    let snark = prove::<_, S>(client, params, None, pk);
+    agg_circuit_mock(agg_circuit_params, snark);
+}

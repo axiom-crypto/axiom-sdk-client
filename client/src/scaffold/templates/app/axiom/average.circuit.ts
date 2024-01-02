809564ca9826021194fc33f8bef3e44c092bc6e2
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-01 23:41:35 -0500

    Update circuit

--- client/src/scaffold/templates/app/axiom/average.circuit.ts
+++ client/src/scaffold/templates/app/axiom/average.circuit.ts
@@ -1,5 +1,7 @@
 import {
   add,
+  sum,
+  div,
   addToCallback,
   CircuitValue,
   CircuitValue256,
@@ -14,25 +16,30 @@ export interface CircuitInputs {
   address: CircuitValue;
 }
 
-export const circuit = async ({
+export const average = async ({
   blockNumber,
   address,
 }: CircuitInputs) => {
-  // Since the blockNumber is a variable input, let's add it to the results that will be sent to my callback function:
+  // Since the blockNumber is a variable input, let's add it to the results that will be sent to our callback function:
   addToCallback(blockNumber);
   addToCallback(address);
 
-  const account = getAccount(blockNumber, address);
+  // number of samples
+  const samples = 10; 
+
+  // number of blocks between each sample
+  const spacing = 100;
 
   // We add a subquery for the account nonce. Note you need to `await`!
-  const bigNonce = await account.nonce();
-
-  // The default return value is `CircuitValue256`, but the nonce should always fit in `uint128` so we can cast it:
-  const nonce = bigNonce.toCircuitValue();
-
-  // As an example of additional compute, we increment the nonce by 1
-  const incNonce = add(nonce, constant(1));
+  const blockNumbers = Array.from({length: samples}, (_: any, i: number) => blockNumber - (spacing * i));
+  const balances = await Promise.all(
+    blockNumbers.map(
+      async (sampleBlockNum: number) => await getAccount(sampleBlockNum, address).balance()
+    )
+  );
+  const total = sum(balances);
+  const average = div(total, samples);
 
   // We add the result to the callback
-  addToCallback(incNonce);
+  addToCallback(average);
 };

638a9171473ac2207d1de1bab352c4a288bcd692
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-05 14:51:17 -0500

    Update harness parsing

---
+++ harness/test/unit/sepolia/output/header/8_state_root.circuit.ts
@@ -0,0 +1,28 @@
+import {
+CircuitValue,
+CircuitValue256,
+getHeader,
+addToCallback,
+add,
+or,
+} from "@axiom-crypto/client";
+
+export const inputs = {
+"claimedBlockNumber":3000000
+};
+export type CircuitInputType = typeof inputs;
+export interface CircuitInputs extends CircuitInputType { }
+export interface CircuitValueInputs {
+  claimedBlockNumber: CircuitValue;
+}
+export const circuit = async ({
+  claimedBlockNumber,
+}: CircuitValueInputs) => {
+
+  for(let i=0; i<8; i++){
+      const header = getHeader(add(3000000, i));
+      addToCallback(await header.stateRoot())
+  }
+  
+  
+};

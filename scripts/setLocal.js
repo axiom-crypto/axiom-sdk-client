6877b5f98364cdb1a74726c3303c47a2fd89fbe8
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-16 15:41:44 -0800

    Update CI test

--- scripts/setLocal.js
+++ scripts/setLocal.js
@@ -2,11 +2,10 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 
 let ci = false;
-if (process.argv[2].toLowerCase() === "ci") {
+if (process.argv[2] === "ci") {
   ci = true;
 }
 
-console.log("Is running CI?", ci, process.argv);
 const packageManager = ci ? "npm" : "pnpm";
 const localPrefix = ci ? "file:" : "link:";
 
@@ -64,7 +63,7 @@ function main() {
     fs.writeFileSync(packageJsonPath.slice(1), JSON.stringify(packageJson, null, 2));
 
     // Install dependencies & build 
-    execSync(`cd ${packages[package].path.slice(1)} && ${packageManager} i && ${packageManager} build && cd ..`);
+    execSync(`cd ${packages[package].path.slice(1)} && ${packageManager} i && ${packageManager} run build && cd ..`);
   }
 }
 

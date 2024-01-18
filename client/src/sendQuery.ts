bc48da1449b55aaee52b5cb98cdaf742e38153aa
Roshan <19766713+rpalakkal@users.noreply.github.com>
2024-01-18 14:49:05 -0500

    fix: fee data fetching (#37)
    * fix: fee data fetching
    
    * fix: fetch before passing to qb
    
    * fix: comment

--- client/src/sendQuery.ts
+++ client/src/sendQuery.ts
@@ -21,6 +21,10 @@ export const buildSendQuery = async (input: {
   if (input.options.refundee === undefined) {
     throw new Error("Refundee is required");
   }
+  if (input.options.maxFeePerGas == undefined) {
+    const feeData = await input.axiom.config.provider.getFeeData();
+    input.options.maxFeePerGas = feeData.maxFeePerGas?.toString();
+  }
   const qb: QueryBuilderV2 = query.new(
     undefined,
     input.computeQuery,

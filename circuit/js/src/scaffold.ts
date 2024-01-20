ab2fe58db0e562324e8ba20c583ceab8b395072c
Yu Jiang Tham <yoojbruin@gmail.com>
2024-01-20 04:15:05 -0500

    Add circuit size check for proof

--- circuit/js/src/scaffold.ts
+++ circuit/js/src/scaffold.ts
@@ -1,5 +1,5 @@
 import { CircuitConfig, Halo2LibWasm } from "@axiom-crypto/halo2-lib-js/wasm/web";
-import { base64ToByteArray, byteArrayToBase64, convertToBytes, convertToBytes32 } from "./utils";
+import { base64ToByteArray, byteArrayToBase64, convertBuiltSubqueries, convertToBytes, convertToBytes32 } from "./utils";
 import { concat, zeroHash } from "viem";
 import { AxiomCircuitRunner } from "./circuitRunner";
 import {
@@ -123,6 +123,14 @@ export abstract class AxiomBaseCircuitScaffold<T> extends BaseCircuitScaffold {
     this.config = config;
     this.results = results;
     await this.populateCircuit(inputs);
+
+    // Validate max circuit subquery size
+    const queryBuilder = (this.axiom.query as QueryV2).new();
+    queryBuilder.setBuiltDataQuery({
+      sourceChainId: this.chainId,
+      subqueries: this.dataQuery,
+    });
+
     await this.keygen();
     const vk = this.getHalo2Vk();
     const encoder = new TextEncoder();
@@ -173,18 +181,19 @@ export abstract class AxiomBaseCircuitScaffold<T> extends BaseCircuitScaffold {
     }
     this.resultLen = Math.floor(numUserInstances / 2);
     this.dataQuery = dataQuery;
-    const queryBuilder = (this.axiom.query as QueryV2).new();
-    queryBuilder.setBuiltDataQuery({
-      sourceChainId: this.chainId,
-      subqueries: dataQuery,
-    });
-    if (!queryBuilder.validate()) {
-      throw new Error("Data query validation failed");
-    }
   }
 
   async run(inputs: RawInput<T>) {
     await this.populateCircuit(inputs);
+
+    // Validate data subqueries
+    const queryBuilder = (this.axiom.query as QueryV2).new();
+    const unbuiltSubqueries = await convertBuiltSubqueries(this.provider, this.dataQuery);
+    queryBuilder.append(unbuiltSubqueries);
+    if (!queryBuilder.validate()) {
+      throw new Error("Subquery validation failed")
+    }
+
     this.prove();
     return this.buildComputeQuery();
   }

d20bde1763f2c850a7bd3b3fd0720da41f334e03
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-01-20 00:42:39 -0500

    chore: switch scaffold test to sepolia

--- circuit/js/test/unit/circuits/7_balance.circuit.ts
+++ circuit/js/test/unit/circuits/7_balance.circuit.ts
@@ -1,31 +1,19 @@
-import {
-CircuitValue,
-CircuitValue256,
-getAccount,
-addToCallback,
-add,
-or,
-} from "../../../src";
+import { CircuitValue, CircuitValue256, getAccount, addToCallback, add, or } from "../../../src";
 
 export const inputs = {
-  address: "0x897dDbe14c9C7736EbfDC58461355697FbF70048",
-  claimedBlockNumber: 9173677,
+  address: "0xB392448932F6ef430555631f765Df0dfaE34efF3",
+  claimedBlockNumber: 4917000,
 };
 export type CircuitInputType = typeof inputs;
-export interface CircuitInputs extends CircuitInputType { }
+export interface CircuitInputs extends CircuitInputType {}
 export interface CircuitValueInputs {
   address: CircuitValue;
   claimedBlockNumber: CircuitValue;
 }
-export const circuit = async ({
-  address,
-  claimedBlockNumber,
-}: CircuitValueInputs) => {
-
+export const circuit = async ({ address, claimedBlockNumber }: CircuitValueInputs) => {
   for (let i = 0; i < 7; i++) {
     const acct = getAccount(add(claimedBlockNumber, i), address);
     const balance = await acct.balance();
     addToCallback(balance);
   }
-  
 };
--- circuit/js/test/unit/scaffold.test.ts
+++ circuit/js/test/unit/scaffold.test.ts
@@ -6,7 +6,7 @@ describe("Scaffold", () => {
   test("Build computeQuery", async () => {
     // This circuit gets a single account's balance 7 times and adds them to the results
     const testCircuit = new AxiomBaseCircuit({
-      provider: process.env.PROVIDER_URI_GOERLI as string,
+      provider: process.env.PROVIDER_URI_SEPOLIA as string,
       f: seven_balance_circuit,
       inputSchema: `{
         "address": "CircuitValue",
@@ -15,16 +15,24 @@ describe("Scaffold", () => {
       chainId: 5,
       mock: true,
     });
+
     const defaultInputs = {
-      address: "0x897dDbe14c9C7736EbfDC58461355697FbF70048",
-      claimedBlockNumber: 9173677,
+      address: "0xB392448932F6ef430555631f765Df0dfaE34efF3",
+      claimedBlockNumber: 4017000,
     };
+
     const _artifact = await testCircuit.compile(defaultInputs);
     const computeQuery = await testCircuit.run(defaultInputs);
     const querySchema = testCircuit.getQuerySchema();
-    
-    expect(querySchema).toEqual("0x21320f4358ce83ec05dcbe1be8cc43002f6ab194fb3d014f9046022aa2bd1784");
-    expect(computeQuery.vkey[0]).toEqual("0x0001000009000100000004010000010080000000000000000000000000000000");
-    expect(computeQuery.computeProof.slice(2).slice(0,128)).toEqual(concat([zeroHash, zeroHash]).slice(2));
+
+    expect(querySchema).toEqual(
+      "0x21320f4358ce83ec05dcbe1be8cc43002f6ab194fb3d014f9046022aa2bd1784",
+    );
+    expect(computeQuery.vkey[0]).toEqual(
+      "0x0001000009000100000004010000010080000000000000000000000000000000",
+    );
+    expect(computeQuery.computeProof.slice(2).slice(0, 128)).toEqual(
+      concat([zeroHash, zeroHash]).slice(2),
+    );
   }, 30000);
 });

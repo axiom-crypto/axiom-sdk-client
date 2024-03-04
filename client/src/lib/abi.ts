57c1e54b6eb052b0c4404b16905985ebcc5adcee
Yu Jiang Tham <ytham@users.noreply.github.com>
2024-03-04 08:10:38 -0500

    Functionality moved from `core`: ABI, Contract Addresses, Integration Tests (#65)
    * WIP: abi and addresses
    
    * WIP: Move integration tests from core to client
    
    * Address handler; WIP: integration tests ported from sdk-core
    
    * Update abi getter
    
    * WIP: Integration tests
    
    * Add lib unit tests
    
    * Finish up integration tests
    
    * Local links
    
    * Merge develop
    
    * Add comments for addresses

---
+++ client/src/lib/abi.ts
@@ -0,0 +1,13 @@
+import { AbiType } from "../types";
+import AxiomV2QueryAbi from "./abi/AxiomV2Query.json";
+
+export function getAxiomV2Abi(
+  type: AbiType,
+) {
+  switch (type) {
+    case AbiType.Query:
+      return AxiomV2QueryAbi.abi;
+    default:
+      throw new Error(`No ABI for AbiType ${type}`);
+  }
+}

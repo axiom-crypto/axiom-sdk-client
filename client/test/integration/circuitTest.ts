cd63587d410c0907ff54bbcebc9791420b672a85
Yu Jiang Tham <yoojbruin@gmail.com>
2024-03-11 18:53:06 -0400

    Update workflows

--- client/test/integration/circuitTest.ts
+++ client/test/integration/circuitTest.ts
@@ -3,6 +3,11 @@ import { existsSync, rmSync } from "fs";
 import path from "path";
 
 export async function generateCircuit(circuitPath: string) {
+  const provider = process.env.PROVIDER_URI_SEPOLIA as string;
+  if (!provider) {
+    throw new Error("`PROVIDER_URI_SEPOLIA` environment variable required");
+  }
+  
   const circuitFile = path.resolve(`./test/integration/circuits/${circuitPath}.circuit.ts`);
   const compiledPath = path.resolve(`./test/integration/circuits/output/${circuitPath}.compiled.json`);
   const inputsPath = path.resolve(`./test/integration/circuits/${circuitPath}.inputs.json`);
@@ -10,7 +15,7 @@ export async function generateCircuit(circuitPath: string) {
   if (existsSync(compiledPath)) {
     rmSync(compiledPath);
   }
-  execSync(`node ./dist/cli/index.js circuit compile ${circuitFile} -o ${compiledPath}`, { stdio: 'inherit' });
+  execSync(`node ./dist/cli/index.js circuit compile ${circuitFile} -o ${compiledPath} -p ${provider}`, { stdio: 'inherit' });
 
   let inputs = {};
   if (existsSync(inputsPath)) {

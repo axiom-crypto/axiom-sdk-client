68a316ed65363e5b1813fc2518cb6fb74dc6ccb8
Yu Jiang Tham <yoojbruin@gmail.com>
2024-03-12 00:06:59 -0400

    Update tests

--- client/test/integration/computeQuery.test.ts
+++ client/test/integration/computeQuery.test.ts
@@ -27,33 +27,4 @@ describe("Build ComputeQuery with DataQuery", () => {
     const receipt = await axiom.sendQuery();
     expect(receipt.status).toBe('success');
   }, 90000);
-
-  test("simple computeQuery with dataQuery and address override", async () => {
-    const chainIdOverride = "8453";
-    const addressOverride = "0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
-
-    const axiom = new Axiom({
-      circuit: circuit,
-      compiledCircuit: compiledCircuit,
-      chainId: chainIdOverride,  // Base
-      provider: process.env.PROVIDER_URI_SEPOLIA as string,
-      privateKey: process.env.PRIVATE_KEY_SEPOLIA as string,
-      callback: {
-        target: "0x4A4e2D8f3fBb3525aD61db7Fc843c9bf097c362e",
-      },
-      options: {
-        overrides: {
-          queryAddress: addressOverride,
-        }
-      }
-    });
-    await axiom.init();
-    await axiom.prove(inputs);
-    const args = axiom.getSendQueryArgs();
-    if (!args) {
-      throw new Error("Unable to get sendQuery args.");
-    }
-    expect(args.args[0]).toBe(chainIdOverride)
-    expect(args.address).toBe(addressOverride);
-  }, 90000);
 });

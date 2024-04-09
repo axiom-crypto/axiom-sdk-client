7a5072c8a4cfd98f7c99101bd73cb85054cca53d
Yu Jiang Tham <yoojbruin@gmail.com>
2024-04-09 19:58:51 -0400

    Fix js circuits

--- circuit/test/unit/circuits/7_balance_override.circuit.ts
+++ circuit/test/unit/circuits/7_balance_override.circuit.ts
@@ -5,7 +5,7 @@ import {
     addToCallback,
     add,
     or,
-} from "../../../dist";
+} from "../../../src";
 
 export const inputs = {
     address: "0x897dDbe14c9C7736EbfDC58461355697FbF70048",

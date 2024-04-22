5ab94576e72bcdff3987ac6549b008a482003de2
Yu Jiang Tham <yoojbruin@gmail.com>
2024-04-21 21:41:09 -0400

    Base mainnet support

--- client/src/axiom/utils.ts
+++ client/src/axiom/utils.ts
@@ -10,7 +10,6 @@ import { AbiType, AxiomV2ClientOverrides, AxiomV2SendQueryArgsParams, CircuitInp
 import { createPublicClient, http } from 'viem';
 import { getAxiomV2Abi, getAxiomV2QueryAddress, viemChain } from "../lib";
 import { getChainDefaults } from "../lib/chain";
-import { QueryBuilderClient } from "../queryBuilderClient";
 
 export function validateChainId(chainId: string) {
   switch (chainId) {
